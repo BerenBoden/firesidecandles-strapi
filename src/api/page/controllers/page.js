0b419f64a3d1efe07c0046294531e487325e79a9
berenboden <beren.boden@gmail.com>
2023-06-10 16:16:29 +1200

    Modified src/api/page/controllers/page.js file to also retrieve a nested relation for categories category_tags, when fetching the pages from strapi.

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -1,9 +1,48 @@
-'use strict';
+"use strict";
 
-/**
- * page controller
- */
+const { sanitizeEntity } = require("strapi-utils");
 
-const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = {
+  async find(ctx) {
+    const { strapi } = ctx;
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.page.search(ctx.query);
+    } else {
+      entities = await strapi.services.page.find(ctx.query, [
+        "categories",
+        "categories.category_tags",
+      ]);
+    }
 
-module.exports = createCoreController('api::page.page');
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.page })
+    );
+  },
+
+  async findOne(ctx) {
+    const { strapi } = ctx;
+    const entity = await strapi.services.page.findOne(ctx.params, [
+      "categories",
+      "categories.category_tags",
+    ]);
+    return sanitizeEntity(entity, { model: strapi.models.page });
+  },
+
+  async create(ctx) {
+    const entity = await strapi.services.page.create(ctx.request.body);
+    return sanitizeEntity(entity, { model: strapi.models.page });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.services.page.update({ id }, ctx.request.body);
+    return sanitizeEntity(entity, { model: strapi.models.page });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.services.page.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.page });
+  },
+};
