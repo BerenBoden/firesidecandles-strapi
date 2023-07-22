9d42aa88d8415c532c292c4b90656ef7166c74d7
berenboden <beren.boden@gmail.com>
2023-07-22 14:44:41 +1200

    Modified the controllers/product.js file to get a product by slug, rather than receiving an array by using the filter parameter at request time.

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * product controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController("api::product.product", ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {};
+    query.filters.slug = { $eq: slug };
+    const entity = await strapi.service("api::product.product").find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+
+    results.forEach((result) => {
+      delete result.updatedBy;
+      delete result.createdBy;
+    });
+
+    return this.transformResponse(results[0]);
+  },
+}));
